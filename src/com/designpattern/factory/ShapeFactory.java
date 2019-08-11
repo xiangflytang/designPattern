6919989341728819acc30622eca4ad86c177457d
tony tang <tony.tang@xxx.com>
2019-08-11 20:59:05 +0800

    update the shapeFactory

--- src/com/designpattern/factory/ShapeFactory.java
+++ src/com/designpattern/factory/ShapeFactory.java
@@ -1,24 +1,7 @@
 package com.designpattern.factory;
 
 public abstract class ShapeFactory {
-	
-	public abstract Shape getShape() ;
-	 //使用 getShape 方法获取形状类型的对象
-	   public Shape getShape(String shapeType){
-	      if(shapeType == null){
-	         return null;
-	      }        
-	      if(shapeType.equalsIgnoreCase("CIRCLE")){
-	         return new Circle();
-	      } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
-	         return new Rectangle();
-	      } else if(shapeType.equalsIgnoreCase("SQUARE")){
-	         return new Square();
-	      }
-	      return null;
-	   }
-	  
-	}
 
+	public abstract Shape getShape();
 
-
+}
